86c352b9298fd30e72c4ffc5d02f5e2880263360
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2021-07-24 00:56:13 +0900

    2021.07.24-00:56

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,52 +17,52 @@ resource "aws_key_pair" "seccamp_sshkey1" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYec7BanC3Jio/twmlnyHEvrUjf/pgw7T0Y0vg2rcNRLyRu+4xkeKbjFoD3P0pMI7fLQXRfuIl4y1imRwxCPp/8nf9Yw6C8d4dHALNG/SE4NpKAOWsptM+Pj0bT2pR/qNdgmwIVscDZou3+aQ/x2yQwoXqjJxl8X8Lm5Efi1m2TYuQs9w1ljhZeJiNQiTo1BD2LKVPNHWDhcqFTve9uzDLhNgSLA2sLkjBPE7Tzn2EttvViAla+TuyJNNZr1TUdXg7ImwLDF76bwX4hArBKkGsBzj7zNUeHTX4SPTaUpVmR4bP96Td+rf5xXmq+nq4MtZOOzPOiga5ombdKE1VLt4vKhhfP0KZeO13pXV2jyUlagYKRhMfeA4Xy/ISJkN5w3p7lofwhfnMV4vxRK1UTGGHUbkNt4AY0CSIxYv0k5LHGiI6XtzKsl6Poodkuh4JZlxWJ/Xp4F3j0J4bs3PIh+asyWpKpCH82ozbQHD26UhBJAqoLuVRMZVHm33W+lsBuks= h-shirokura@security-camp2021.jp"
 }
 
-resource "aws_vpc" "slankdev_vpc1" {
+resource "aws_vpc" "seccamp_vpc1" {
   cidr_block           = "10.255.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "false"
 
   tags = {
-    Name = "slankdev_vpc1"
+    Name = "seccamp_vpc1"
   }
 }
 
-resource "aws_internet_gateway" "slankdev_vpc1_igw" {
-  vpc_id = "${aws_vpc.slankdev_vpc1.id}"
+resource "aws_internet_gateway" "seccamp_vpc1_igw" {
+  vpc_id = "${aws_vpc.seccamp_vpc1.id}"
 
   tags = {
-    Name = "slankdev_vpc1_igw"
+    Name = "seccamp_vpc1_igw"
   }
 }
 
-resource "aws_subnet" "slankdev_vpc1_subnet1" {
-  vpc_id            = "${aws_vpc.slankdev_vpc1.id}"
+resource "aws_subnet" "seccamp_vpc1_subnet1" {
+  vpc_id            = "${aws_vpc.seccamp_vpc1.id}"
   cidr_block        = "10.255.1.0/24"
   availability_zone = "ap-northeast-1a"
   map_public_ip_on_launch = "false"
 
   tags = {
-    Name = "slankdev_vpc1_subnet1"
+    Name = "seccamp_vpc1_subnet1"
   }
 }
 
 resource "aws_route_table" "route_table1" {
-  vpc_id = "${aws_vpc.slankdev_vpc1.id}"
+  vpc_id = "${aws_vpc.seccamp_vpc1.id}"
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.slankdev_vpc1_igw.id}"
+    gateway_id = "${aws_internet_gateway.seccamp_vpc1_igw.id}"
   }
 }
 
 resource "aws_route_table_association" "route_association1" {
-  subnet_id = "${aws_subnet.slankdev_vpc1_subnet1.id}"
+  subnet_id = "${aws_subnet.seccamp_vpc1_subnet1.id}"
   route_table_id = "${aws_route_table.route_table1.id}"
 }
 
-resource "aws_security_group" "slankdev_sg1" {
-  vpc_id = "${aws_vpc.slankdev_vpc1.id}"
-  name   = "slankdev_sg1"
+resource "aws_security_group" "seccamp_sg1" {
+  vpc_id = "${aws_vpc.seccamp_vpc1.id}"
+  name   = "seccamp_sg1"
 
   ingress {
     description = "SSH from Any"
@@ -89,37 +89,37 @@ resource "aws_security_group" "slankdev_sg1" {
   }
 
   tags = {
-    Name = "slankdev_sg1"
+    Name = "seccamp_sg1"
   }
 }
 
-resource "aws_launch_template" "slankdev_instance_template1" {
-  name          = "slankdev_instance_template1"
+resource "aws_launch_template" "seccamp_instance_template1" {
+  name          = "seccamp_instance_template1"
   image_id      = "${var.image_ubuntu_2004lts.ap-northeast-1}"
   instance_type = "t2.micro"
   key_name      = "seccamp_sshkey1"
 
   network_interfaces {
     associate_public_ip_address = "false"
-    subnet_id = "${aws_subnet.slankdev_vpc1_subnet1.id}"
+    subnet_id = "${aws_subnet.seccamp_vpc1_subnet1.id}"
     security_groups = [
-      "${aws_security_group.slankdev_sg1.id}"
+      "${aws_security_group.seccamp_sg1.id}"
     ]
   }
 }
 
-resource "aws_instance" "slankdev_instance_student" {
+resource "aws_instance" "seccamp_instance_student" {
   count = "${var.student_count}"
-  launch_template { id = "${aws_launch_template.slankdev_instance_template1.id}" }
+  launch_template { id = "${aws_launch_template.seccamp_instance_template1.id}" }
   tags = { Name = "${format("student%02d", count.index + 1)}" }
 }
 
-resource "aws_eip" "slankdev_instance_student" {
+resource "aws_eip" "seccamp_instance_student" {
   count = "${var.student_count}"
-  instance = "${element(aws_instance.slankdev_instance_student.*.id, count.index)}"
+  instance = "${element(aws_instance.seccamp_instance_student.*.id, count.index)}"
   vpc      = true
 }
 
 output "student-servers" {
-  value = "${aws_instance.slankdev_instance_student.*.public_ip}"
+  value = "${aws_instance.seccamp_instance_student.*.public_ip}"
 }
