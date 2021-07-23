0fee02516f846f4d0a8812c1b37e2dacf4978a42
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2021-07-24 00:55:05 +0900

    2021.07.24-00:55

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,4 @@
-variable "ImagesUbuntu2004LTS" {
+variable "image_ubuntu_2004lts" {
   default = {
     ap-northeast-1 = "ami-0827d8ed0295e3feb"
   }
@@ -12,57 +12,57 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_key_pair" "SlankdevSshKey1" {
-  key_name   = "SlankdevSshKey1"
+resource "aws_key_pair" "seccamp_sshkey1" {
+  key_name   = "seccamp_sshkey1"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYec7BanC3Jio/twmlnyHEvrUjf/pgw7T0Y0vg2rcNRLyRu+4xkeKbjFoD3P0pMI7fLQXRfuIl4y1imRwxCPp/8nf9Yw6C8d4dHALNG/SE4NpKAOWsptM+Pj0bT2pR/qNdgmwIVscDZou3+aQ/x2yQwoXqjJxl8X8Lm5Efi1m2TYuQs9w1ljhZeJiNQiTo1BD2LKVPNHWDhcqFTve9uzDLhNgSLA2sLkjBPE7Tzn2EttvViAla+TuyJNNZr1TUdXg7ImwLDF76bwX4hArBKkGsBzj7zNUeHTX4SPTaUpVmR4bP96Td+rf5xXmq+nq4MtZOOzPOiga5ombdKE1VLt4vKhhfP0KZeO13pXV2jyUlagYKRhMfeA4Xy/ISJkN5w3p7lofwhfnMV4vxRK1UTGGHUbkNt4AY0CSIxYv0k5LHGiI6XtzKsl6Poodkuh4JZlxWJ/Xp4F3j0J4bs3PIh+asyWpKpCH82ozbQHD26UhBJAqoLuVRMZVHm33W+lsBuks= h-shirokura@security-camp2021.jp"
 }
 
-resource "aws_vpc" "slankdev-test-vpc-1" {
+resource "aws_vpc" "slankdev_vpc1" {
   cidr_block           = "10.255.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "false"
 
   tags = {
-    Name = "slankdev-test-vpc-1"
+    Name = "slankdev_vpc1"
   }
 }
 
-resource "aws_internet_gateway" "slankdev-test-vpc-1-igw" {
-  vpc_id = "${aws_vpc.slankdev-test-vpc-1.id}"
+resource "aws_internet_gateway" "slankdev_vpc1_igw" {
+  vpc_id = "${aws_vpc.slankdev_vpc1.id}"
 
   tags = {
-    Name = "slankdev-test-vpc-1-igw"
+    Name = "slankdev_vpc1_igw"
   }
 }
 
-resource "aws_subnet" "slankdev-test-vpc-1-subnet-1" {
-  vpc_id            = "${aws_vpc.slankdev-test-vpc-1.id}"
+resource "aws_subnet" "slankdev_vpc1_subnet1" {
+  vpc_id            = "${aws_vpc.slankdev_vpc1.id}"
   cidr_block        = "10.255.1.0/24"
   availability_zone = "ap-northeast-1a"
   map_public_ip_on_launch = "false"
 
   tags = {
-    Name = "slankdev-test-vpc-1-subnet-1"
+    Name = "slankdev_vpc1_subnet1"
   }
 }
 
-resource "aws_route_table" "public-route" {
-  vpc_id = "${aws_vpc.slankdev-test-vpc-1.id}"
+resource "aws_route_table" "route_table1" {
+  vpc_id = "${aws_vpc.slankdev_vpc1.id}"
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.slankdev-test-vpc-1-igw.id}"
+    gateway_id = "${aws_internet_gateway.slankdev_vpc1_igw.id}"
   }
 }
 
-resource "aws_route_table_association" "puclic-route-association" {
-  subnet_id = "${aws_subnet.slankdev-test-vpc-1-subnet-1.id}"
-  route_table_id = "${aws_route_table.public-route.id}"
+resource "aws_route_table_association" "route_association1" {
+  subnet_id = "${aws_subnet.slankdev_vpc1_subnet1.id}"
+  route_table_id = "${aws_route_table.route_table1.id}"
 }
 
-resource "aws_security_group" "slankdev-sg-1" {
-  vpc_id = "${aws_vpc.slankdev-test-vpc-1.id}"
-  name   = "slankdev-sg-1"
+resource "aws_security_group" "slankdev_sg1" {
+  vpc_id = "${aws_vpc.slankdev_vpc1.id}"
+  name   = "slankdev_sg1"
 
   ingress {
     description = "SSH from Any"
@@ -89,21 +89,21 @@ resource "aws_security_group" "slankdev-sg-1" {
   }
 
   tags = {
-    Name = "slankdev-sg-1"
+    Name = "slankdev_sg1"
   }
 }
 
 resource "aws_launch_template" "slankdev_instance_template1" {
   name          = "slankdev_instance_template1"
-  image_id      = "${var.ImagesUbuntu2004LTS.ap-northeast-1}"
+  image_id      = "${var.image_ubuntu_2004lts.ap-northeast-1}"
   instance_type = "t2.micro"
-  key_name      = "SlankdevSshKey1"
+  key_name      = "seccamp_sshkey1"
 
   network_interfaces {
     associate_public_ip_address = "false"
-    subnet_id = "${aws_subnet.slankdev-test-vpc-1-subnet-1.id}"
+    subnet_id = "${aws_subnet.slankdev_vpc1_subnet1.id}"
     security_groups = [
-      "${aws_security_group.slankdev-sg-1.id}"
+      "${aws_security_group.slankdev_sg1.id}"
     ]
   }
 }
